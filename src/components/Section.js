27946371f884da9adc23788ff287ff4b6b47aac8
Shawn <shawn17@live.fr>
2022-06-26 19:19:56 +0200

    ✨ Add animation to the Section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Fade } from 'react-reveal'
 
 export default function Section({ title, description, image, leftBtnText, rightBtnText }) {
   return (
     // Pass props to the component
     <Wrap bgImage={image}>
-      <ItemText>
-        <h1>
-          { title }
-        </h1>
-        <p>
-          { description }
-        </p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>
+            { title }
+          </h1>
+          <p>
+            { description }
+          </p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>
-            { leftBtnText }
-          </LeftButton>
-          {rightBtnText && 
-            <RightButton>
-            { rightBtnText }
-            </RightButton>
-          }
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>
+              { leftBtnText }
+            </LeftButton>
+            {rightBtnText && 
+              <RightButton>
+              { rightBtnText }
+              </RightButton>
+            }
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="../assets/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
