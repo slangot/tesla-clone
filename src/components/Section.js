2be5b3e0a07ef204f5df062568285ef1111b23b1
Shawn <shawn17@live.fr>
2022-06-26 18:16:30 +0200

    💄 Add static style to Section component; add new icons and images assets

--- src/components/Section.js
+++ src/components/Section.js
@@ -12,11 +12,26 @@ export default function Section() {
           Order Online for Touchless Delivery
         </p>
       </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>
+            Custom Order
+          </LeftButton>
+          <RightButton>
+            Existing Inventory
+          </RightButton>
+        </ButtonGroup>
+        <DownArrow src="../assets/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   )
 }
 
 const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   width: 100vw;
   height: 100vh;
   background-size: cover;
@@ -30,3 +45,42 @@ const ItemText = styled.div`
   text-align: center;
 `
 
+const Buttons = styled.div`
+
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`
+
+// Heritage from LeftButton Style
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s
+`
