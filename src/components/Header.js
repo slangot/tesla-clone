83e462dd2bb63759d1eb40b4e320d06754a573bd
Shawn <shawn17@live.fr>
2022-06-26 19:19:17 +0200

    ✨ Header creation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,76 @@
 import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <Container>
+      <a href="#">
+        <img src="../assets/images/logo.svg" alt="" />
+      </a>
+      <LeftMenu>
+          <a href="#">
+            Model S
+          </a>
+          <a href="#">
+            Model 3
+          </a>
+          <a href="#">
+            Model X
+          </a>
+          <a href="#">
+            Model Y
+          </a>
+      </LeftMenu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <SideMenu />
+      </RightMenu>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`
+
+const LeftMenu = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+  }
+
+  @media(max-width: 768px) {
+    display: none;
+  }
+`
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`
+
+const SideMenu = styled(MenuIcon)`
+  cursor: pointer;
+`
