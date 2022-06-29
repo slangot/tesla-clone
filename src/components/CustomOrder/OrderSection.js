351de84bc31300f6592037ee0ff86d8c60e9c9e4
Shawn <shawn17@live.fr>
2022-06-29 10:59:02 +0200

    ✨ Add slider info bubble and its style

--- src/components/CustomOrder/OrderSection.js
+++ src/components/CustomOrder/OrderSection.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 import InfoIcon from '@mui/icons-material/Info';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 export default function OrderSection() {
   return (
     <Container>
       <SliderContainer>
         <img src="../../assets/images/model-3-1-min.png" alt="model 3" />
-        <SliderInfoContainer>
-
-        </SliderInfoContainer>
+        <SliderToastContainer>
+          <ArrowIconContainer>
+            <ArrowIcon sx={{ fontSize:"15px" }}/>
+          </ArrowIconContainer>
+          <InfoSliderContainer>
+            <InfoSliderContainerFirst>
+              <h3>A partir de 428,44€ <span className='smallInfo'>/mois* LOA</span></h3>
+              <span className='divider'></span>
+              <p>A partir de - 75 € /mois Économies de carburant estimées</p>
+            </InfoSliderContainerFirst>
+            <InfoSliderContainerSecond>
+              <p>
+              1er loyer majoré de 11 900 €. Un credit vous engage et doit être remboursé. Vérifiez vos capacités de remboursement avant de vous engager
+              </p>
+            </InfoSliderContainerSecond>
+          </InfoSliderContainer>
+        </SliderToastContainer>
       </SliderContainer>
       <InfoContainer>
         <InfoBubble>
@@ -58,16 +73,80 @@ const SliderContainer = styled.div`
   background-image: ${props => `url("${props.bgImage}")`}; */
 `
 
-const SliderInfoContainer = styled.div`
+const SliderToastContainer = styled.div`
   position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   width: 70%;
   margin: 0 auto;
   bottom: 0;
+  padding: 15px;
   left: 50%;
   transform: translateX(-50%);
   background-color: rgba(180,180,180,0.1);
   border-radius: 10px 10px 0px 0px;
   height: 80px;
+`
+
+const ArrowIconContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  align-content: center;
+  padding: 15px;
+  width: 20px;
+  height: 20px;
+  background-color: white;
+  border-radius: 50%;
+  box-shadow: 0px 3px 3px rgba(0,0,0,0.1);
+`
+
+const ArrowIcon = styled(ArrowBackIosIcon)`
+position: absolute;
+top: 50%;
+transform: rotate(90deg) translateX(-25%);
+`
+
+const InfoSliderContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-self: center;
+  margin: 0 15px;
+`
+
+const InfoSliderContainerFirst = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  h3 {
+    font-size: 0.7rem;
+  }
+  .smallInfo {
+    font-size: 0.6rem;
+    font-weight: normal;
+  }
+  p{
+    font-size: 0.6rem;
+  }
+  .divider {
+    height: 15px;
+    width: 2px;
+    margin: 0 20px;
+    background-color: rgba(0,0,0,0.1);
+  }
+`
+
+const InfoSliderContainerSecond = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  margin-top: 10px;
+  p {
+    font-size: 0.6rem;
+  }
 `
 
 const InfoContainer = styled.div`
